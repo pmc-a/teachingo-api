1ffd440b290b861202f8eceba7d5ce0ea6c8ba41
Chloe McAteer <chloemcateer97@gmail.com>
2020-04-06 21:03:16 +0100

    Unit test added for status endpoint

---
+++ src/tests/server.test.js
@@ -0,0 +1,14 @@
+const { app } = require('../server');
+const request = require('supertest');
+
+describe( 'server', () => {
+    it('should return 200 from the status endpoint', done => {
+        request(app)
+        .get('/api/status')
+        .expect(200)
+        .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
+    });
+});
