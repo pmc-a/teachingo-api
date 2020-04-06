89981ef8b9e00397ab84c717fd93337e117230ae
Peter McAree <me@petermcaree.com>
2020-04-06 20:28:52 +0100

    add: status endpoint for health check

--- src/index.js
+++ src/index.js
@@ -10,21 +10,24 @@ const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
+const port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/token', (req, res) => {
-  const { identity, roomName } = req.query;
-  const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
-    ttl: MAX_ALLOWED_SESSION_DURATION,
-  });
-  token.identity = identity;
-  const videoGrant = new VideoGrant({ room: roomName });
-  token.addGrant(videoGrant);
-  res.send(token.toJwt());
-  console.log(`issued token for ${identity} in room ${roomName}`);
+app.get('/api/status', (req, res) => {
+  res.status(200).end();
 });
 
-app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
+// app.get('/token', (req, res) => {
+//   const { identity, roomName } = req.query;
+//   const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+//     ttl: MAX_ALLOWED_SESSION_DURATION,
+//   });
+//   token.identity = identity;
+//   const videoGrant = new VideoGrant({ room: roomName });
+//   token.addGrant(videoGrant);
+//   res.send(token.toJwt());
+//   console.log(`issued token for ${identity} in room ${roomName}`);
+// });
 
-app.listen(8081, () => console.log('token server running on 8081'));
-
+app.listen(port, () => console.log(`Teachingo API service running on ${port}`));
