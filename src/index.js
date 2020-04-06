c67f368da0a5e7889cc1aaf2a83042f5553bcaaa
Chloe McAteer <chloemc@kainos.com>
2020-04-06 21:06:46 +0100

    Unit test added for status endpoint (#2)
    * Unit test added for status endpoint
    
    * Removed flag from jest start script

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const AccessToken = require('twilio').jwt.AccessToken;
-const VideoGrant = AccessToken.VideoGrant;
-require('dotenv').config();
-
-const MAX_ALLOWED_SESSION_DURATION = 14400;
-const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
-const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
-
+const { app } = require('./server');
 const port = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/api/status', (req, res) => {
-  res.status(200).end();
-});
-
-// app.get('/token', (req, res) => {
-//   const { identity, roomName } = req.query;
-//   const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
-//     ttl: MAX_ALLOWED_SESSION_DURATION,
-//   });
-//   token.identity = identity;
-//   const videoGrant = new VideoGrant({ room: roomName });
-//   token.addGrant(videoGrant);
-//   res.send(token.toJwt());
-//   console.log(`issued token for ${identity} in room ${roomName}`);
-// });
 
 app.listen(port, () => console.log(`Teachingo API service running on ${port}`));
---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+require('dotenv').config();
+
+const MAX_ALLOWED_SESSION_DURATION = 14400;
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
+const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
+
+app.get('/api/status', (req, res) => {
+  res.status(200).end();
+});
+
+// app.get('/token', (req, res) => {
+//   const { identity, roomName } = req.query;
+//   const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+//     ttl: MAX_ALLOWED_SESSION_DURATION,
+//   });
+//   token.identity = identity;
+//   const videoGrant = new VideoGrant({ room: roomName });
+//   token.addGrant(videoGrant);
+//   res.send(token.toJwt());
+//   console.log(`issued token for ${identity} in room ${roomName}`);
+// });
+
+module.exports = { app };
