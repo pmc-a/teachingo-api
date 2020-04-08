15d0e399ad2965c9ee343a69aed35127411e3ecb
Chloe McAteer <chloemcateer97@gmail.com>
2020-04-08 11:19:14 +0100

    Knex added to project and user table created

---
+++ migrations/20200408111124_create_user_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments('id');
+        table.string('email', 255).notNullable();
+        table.string('password', 255).notNullable();
+        table.string('first_name', 255).notNullable();
+        table.string('last_name', 255).notNullable();
+        table.string('mobile', 255).notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
