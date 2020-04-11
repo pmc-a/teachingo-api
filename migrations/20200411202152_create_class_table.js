5dfd25697ec23ba03f504ffefab4e6d88c1a397c
Chloe McAteer <cmcateer31@qub.ac.uk>
2020-04-11 22:01:39 +0100

    Added classes table and some seed data (#9)
    * Added classes table and some seed data
    
    * Updated scripts to run seeds

---
+++ migrations/20200411202152_create_class_table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('classes', (table) => {
+        table.unique(['class_id', 'student_id']);
+        table.string('class_id', 255).notNullable();
+        table
+            .integer('student_id')
+            .references('id')
+            .inTable('users')
+            .notNull()
+            .onDelete('cascade');
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('classes');
+};
