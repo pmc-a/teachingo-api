ac1da5c0fdca6f0913e843f4352dd91885c992b7
Peter McAree <me@petermcaree.com>
2020-04-26 17:24:02 +0100

    add: socket io to facilitate live chat feature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const { app } = require('./server');
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Teachingo API service running on ${port}`));
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('New incoming msg:', msg);
+        io.emit('new chat message', msg);
+    });
+});
+
+http.listen(port, () =>
+    console.log(`Teachingo API service running on ${port}`)
+);
